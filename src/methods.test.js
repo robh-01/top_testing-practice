f332b2569a7c79effea6387ba581f49a587f6698
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2024-12-13 18:30:41 +0545

    Make the testing comments better

--- src/methods.test.js
+++ src/methods.test.js
@@ -1,18 +1,41 @@
 import { capitalize, reverseString } from "./methods.js";
 
-test("function 'capitalize' works", () => {
-  expect(capitalize("hello")).toBe("Hello");
-  expect(capitalize("Hello")).toBe("Hello");
-  expect(capitalize("my name is ROSHAN")).toBe("My name is ROSHAN");
-  expect(capitalize("-Roshan Bhusal")).toBe("-Roshan Bhusal");
+describe("'capitalize' function works", () => {
+  test("capitalizes a simple word", () => {
+    expect(capitalize("hello")).toBe("Hello");
+  });
+  test("keeps the already capitalized word as it is", () => {
+    expect(capitalize("Hello")).toBe("Hello");
+  });
+  test("capitalizes the first letter in the sentence", () => {
+    expect(capitalize("my name is Roshan")).toBe("My name is Roshan");
+  });
+  test("leaves the characters which are not letters as it is", () => {
+    expect(capitalize("-Roshan Bhusal")).toBe("-Roshan Bhusal");
+  });
 });
 
-test("function 'reverseString' works", () => {
-  expect(reverseString("hello")).toBe("olleh");
-  expect(reverseString("Roshan")).toBe("nahsoR");
-  expect(reverseString("1587")).toBe("7851");
-  expect(reverseString("excited!!")).toBe("!!deticxe");
-  expect(reverseString("")).toBe("");
-  expect(reverseString("a")).toBe("a");
-  expect(reverseString(" spaces ")).toBe(" secaps ");
+
+describe("'reverseString' function works", () => {
+  test("reverses a simple word", () => {
+    expect(reverseString("hello")).toBe("olleh");
+  });
+  test("reverses a word with capital letters", () => {
+    expect(reverseString("Roshan")).toBe("nahsoR");
+  });
+  test("reverses numeric strings", () => {
+    expect(reverseString("1587")).toBe("7851");
+  });
+  test("reverses a string with special characters", () => {
+    expect(reverseString("excited!!")).toBe("!!deticxe");
+  });
+  test("reverses an empty string", () => {
+    expect(reverseString("")).toBe("");
+  });
+  test("reverses a single character string", () => {
+    expect(reverseString("a")).toBe("a");
+  });
+  test("reverses a string with spaces", () => {
+    expect(reverseString(" spaces ")).toBe(" secaps ");
+  });
 });
